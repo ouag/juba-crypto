4a85d31d64e1c1ce47c356eeae4aed398ccf3b02
ouagoughi mo rabah <mo.ouag@ouagoughis-macbook-pro.home>
2018-06-10 19:33:43 +0200

    project creation

---
+++ dynamodb-repository/dynamodb-repository-api/src/main/java/com/ouag/juba/dynamodb/DynamoDBRepositoryApplication.java
@@ -0,0 +1,15 @@
+package com.ouag.juba.dynamodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class DynamoDBRepositoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynamoDBRepositoryApplication.class, args);
+    }
+
+}
+
